073476bdb141e3f313d1bcd8552148d72a5577cd
jader <jader@host.docker.internal>
2020-09-07 14:43:47 -0300

    Registrando no Discovery Service e efetuando a consulta

---
+++ discovery/src/test/java/microservice/learning/com/discovery/ApplicationTests.java
@@ -0,0 +1,13 @@
+package microservice.learning.com.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
